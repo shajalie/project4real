5b8629e5e82ddd00dc2c454d4666a14d190b49aa
ahajalie <ahajalie@umich.edu>
2015-04-13 20:37:38 -0400

    stuff

--- eecs484p4/eecs484p4/StudentComponent/LogMgr.cpp
+++ eecs484p4/eecs484p4/StudentComponent/LogMgr.cpp
@@ -3,15 +3,16 @@
 
 #include "LogMgr.h"
 
+using namespace std;
 
-int LogNgr::getLastLSN(int txnum) {
+int LogMgr::getLastLSN(int txnum) {
 	// not sure, get the lastest that is in the logtail
 	// if not get the lastest one in the log file?
 	if(tx_table.count(txnum) > 0) {
-		return tx_table[txnum].lastSLN;
+		return tx_table[txnum].lastLSN;
 	}
 	else {
-		return null;
+		return NULL_TX;
 	}
 	//testasassa
 }
@@ -25,21 +26,14 @@ void LogMgr::setLastLSN(int txnum, int lsn) {
 
 }
 
-// who knows
-void LogMgr::setStorageEngine(StorageEngine* engine) {
-	se = engine;
-}
-
-void LogMgr::checkpoint() {
-	// write begin checkpoint to log file?
-	int lsn = se.nextLSN();
-	int prevLSN = getLastLSN(txid);
-	logtail.push_back(new LogRecord(lsn, prevLSN, BEGIN_CKPT));
-
-	// done with begin checkpint?
-
-	// end checkpoint
-
+void LogMgr::flushLogTail(int maxLSN) {
+	for(int i = 0; i < logtail.size(); ++i) {
+		if(logtail[i]->getLSN() < maxLSN) {
+			se->updateLog(logtail[i]->toString());
+			logtail.erase(logtail.begin() + i);
+			i--;
+		}
+	}
 }
 
 void LogMgr::analyze(vector <LogRecord*> log) {
@@ -47,16 +41,62 @@ void LogMgr::analyze(vector <LogRecord*> log) {
 
 }
 
+bool LogMgr::redo(vector <LogRecord*> log) {
+
+}
+
+void LogMgr::undo(vector <LogRecord*> log, int txnum) {
+	
+}
+
+void LogMgr::abort(int txid) {
+
+}
+
+void LogMgr::checkpoint() {
+	// write begin checkpoint to log file?
+	// int lsn = se.nextLSN();
+	// int prevLSN = getLastLSN(txid);
+	// logtail.push_back(new LogRecord(lsn, prevLSN, BEGIN_CKPT));
+
+	// done with begin checkpint?
+
+	// end checkpoint
+
+}
+
+void LogMgr::commit(int txid) {
+
+}
+
+void LogMgr::pageFlushed(int page_id) {
+
+}
+
+void LogMgr::recover(string log) {
+
+}
+
 int LogMgr::write(int txid, int page_id, int offset, string input, string oldtext) {
 
 	// check point is where the writes go to the DB?
 	// so update the log on this write?
-	int lsn = se.nextLSN();
-	int prevLSN = getLastLSN(txid);
-	logtail.push_back(new UpdateLogRecord(lsn, prevLSN, 
-		txid, page_id, offset, oldtxt, input));
+	// int lsn = se.nextLSN();
+	// int prevLSN = getLastLSN(txid);
+	// logtail.push_back(new UpdateLogRecord(lsn, prevLSN, 
+	// 	txid, page_id, offset, oldtxt, input));
 
-	// who knows
-	updatePage(page_id, offset, input);
+	// // who knows
+	// updatePage(page_id, offset, input);
 
 }
+
+// who knows
+void LogMgr::setStorageEngine(StorageEngine* engine) {
+	LogMgr::se = engine;
+}
+
+
+
+
+
